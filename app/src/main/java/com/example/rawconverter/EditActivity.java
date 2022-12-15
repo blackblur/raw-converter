a28be4e80c210ad22064bb879bfa505434ed86be
Stefanie Nunes <stefanie.nunes@hotmail.de>
2022-12-16 00:33:29 +0100

    Initial interface layout

---
+++ app/src/main/java/com/example/rawconverter/EditActivity.java
@@ -0,0 +1,95 @@
+package com.example.rawconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EditActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    LinearLayout whiteOptions, toneOptions;
+    Switch whiteSwitch, toneSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        LibRaw libraw = LibRaw.newInstance();
+
+        // Get view elements
+        imageView = findViewById(R.id.imageView);
+        whiteOptions = findViewById(R.id.white_options);
+        toneOptions = findViewById(R.id.tone_options);
+        whiteSwitch = findViewById(R.id.white_switch);
+        toneSwitch = findViewById(R.id.tone_switch);
+
+        // Hide options
+        whiteOptions.setVisibility(View.GONE);
+        toneOptions.setVisibility(View.GONE);
+
+        // Get bitmap
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Uri imagePath = (Uri) extras.get("imagePath");
+
+            InputStream iStream = null;
+            byte[] inputData = null;
+
+            try {
+                iStream = getContentResolver().openInputStream(imagePath);
+                inputData = getBytes(iStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (inputData != null) {
+                int status = libraw.openBuffer(inputData, inputData.length);
+                Bitmap bitmap = libraw.decodeAsBitmap(true);
+                if (bitmap != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+
+            libraw.close();
+        }
+
+        // Wire toggle switches
+        whiteSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                whiteOptions.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+        toneSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                toneOptions.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+    }
+
+    public static byte[] getBytes(InputStream inputStream) throws IOException {
+        // https://stackoverflow.com/questions/10296734/image-uri-to-bytesarray
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
+    }
+}
--- app/src/main/java/com/example/rawconverter/MainActivity.java
+++ app/src/main/java/com/example/rawconverter/MainActivity.java
@@ -7,37 +7,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import com.example.rawconverter.LibRaw;
-
 public class MainActivity extends AppCompatActivity {
 
     ActivityResultLauncher<String> openRaw;
     ImageView thumbnail;
+    Button continueButton;
+
+    Uri imagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Libraw Class test
-        LibRaw libraw = LibRaw.newInstance();
-        Log.i("C++ String", libraw.stringFromJNI());
-
-        thumbnail = (ImageView) findViewById(R.id.thumbnail);
 
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -50,48 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onActivityResult(Uri result) {
-
-                        InputStream iStream = null;
-                        byte[] inputData = null;
-
-                        try {
-                            iStream = getContentResolver().openInputStream(result);
-                            inputData = getBytes(iStream);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                        if (inputData != null) {
-                            int status = libraw.openBuffer(inputData, inputData.length);
-                            Bitmap bitmap = libraw.decodeAsBitmap(true);
-//                            Bitmap bitmap = BitmapFactory.decodeByteArray(res, 0, res.length);
-                            if (bitmap != null) {
-                                thumbnail.setImageBitmap(bitmap);
-                            }
-                        }
-
-                        libraw.close();
+                        Intent editIntent = new Intent(MainActivity.this, EditActivity.class);
+                        editIntent.putExtra("imagePath", result);
+                        startActivity(editIntent);
                     }
                 }
         );
-
     }
 
     public void loadRawFile(View v) {
         openRaw.launch("*/*");
-
     }
 
-    public byte[] getBytes(InputStream inputStream) throws IOException {
-        // https://stackoverflow.com/questions/10296734/image-uri-to-bytesarray
-        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-        int bufferSize = 1024;
-        byte[] buffer = new byte[bufferSize];
-
-        int len = 0;
-        while ((len = inputStream.read(buffer)) != -1) {
-            byteBuffer.write(buffer, 0, len);
-        }
-        return byteBuffer.toByteArray();
-    }
 }
