cf9e1dbc9e20e7f7366daa0656affef675d65ef5
Stefanie Nunes <stefanie.nunes@hotmail.de>
2023-01-17 01:21:30 +0100

    enhance ui

--- app/src/main/java/com/example/rawconverter/EditActivity.java
+++ app/src/main/java/com/example/rawconverter/EditActivity.java
@@ -1,6 +1,8 @@
 package com.example.rawconverter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -8,6 +10,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -16,28 +19,34 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarItemView;
+import com.google.android.material.navigation.NavigationBarView;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Logger;
 
 public class EditActivity extends AppCompatActivity {
 
+    int toneSelection, curveSelection;
+
     LibRaw libraw;
     ImageView imageView;
-    LinearLayout whiteOptions, toneOptions;
-    Switch whiteSwitch, toneSwitch;
+    Group whiteBalancingGroup, toneMappingGroup, extraGroup;
     Button processButton;
     ProgressBar progressCircle;
-    CheckBox cameraWbCheck;
-    CheckBox autoWbCheck;
-    CheckBox noWbCheck;
-    SeekBar brightnessSeek;
-    SeekBar gammaSeek;
+    RadioGroup whiteBalancingRadioGroup, toneRadioGroup, curveRadioGroup;
+    SeekBar brightnessSeek, gammaSeek;
     ToneCurveView toneCurveView;
+    NavigationBarView bottomNavigation;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -47,25 +56,29 @@ public class EditActivity extends AppCompatActivity {
 
         libraw = LibRaw.newInstance();
 
+        // Set default radio selections
+        toneSelection = 0;
+        curveSelection = 0;
+
         // Get view elements
         imageView = findViewById(R.id.imageView);
-        whiteOptions = findViewById(R.id.white_options);
-        toneOptions = findViewById(R.id.tone_options);
-        whiteSwitch = findViewById(R.id.white_switch);
-        toneSwitch = findViewById(R.id.tone_switch);
+        whiteBalancingGroup = findViewById(R.id.white_balancing_group);
+        toneMappingGroup = findViewById(R.id.tone_mapping_group);
+        extraGroup = findViewById(R.id.extra_group);
         progressCircle = findViewById(R.id.img_loading_circle);
         processButton = findViewById(R.id.process_btn);
-        cameraWbCheck = findViewById(R.id.checkBox_camera_white_balance);
-        autoWbCheck = findViewById(R.id.checkBox_auto_white_balance);
-        noWbCheck = findViewById(R.id.checkBox_no_white_balance);
+        whiteBalancingRadioGroup = findViewById(R.id.radio_group_white_balancing);
+        toneRadioGroup = findViewById(R.id.radio_group_tone);
+        curveRadioGroup = findViewById(R.id.radio_group_curve);
         brightnessSeek = findViewById(R.id.brightness_seekbar);
         gammaSeek = findViewById(R.id.gamma_seekbar);
         toneCurveView = findViewById(R.id.tone_curve);
-
+        bottomNavigation = findViewById(R.id.bottom_navigation);
 
         // Hide options
-        whiteOptions.setVisibility(View.GONE);
-        toneOptions.setVisibility(View.GONE);
+        whiteBalancingGroup.setVisibility(View.VISIBLE);
+        toneMappingGroup.setVisibility(View.GONE);
+        extraGroup.setVisibility(View.GONE);
 
         // Get bitmap
         Bundle extras = getIntent().getExtras();
@@ -104,6 +117,7 @@ public class EditActivity extends AppCompatActivity {
             }.start();
         }
 
+        // Wire process button
         processButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -111,52 +125,68 @@ public class EditActivity extends AppCompatActivity {
             }
         });
 
-        // Wire toggle switches
-        whiteSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                whiteOptions.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+        // Wire radio groups
+        whiteBalancingRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                // TODO: Check user mul values
+                if (findViewById(i) == findViewById(R.id.radio_no_white_balance)) {
+                    libraw.setUserMul(new float[]{1f, 1f, 1f, 1f});
+                    libraw.setCameraWb(false);
+                    libraw.setAutoWb(false);
+                } else if (findViewById(i) == findViewById(R.id.radio_camera_white_balance)) {
+                    libraw.setUserMul(new float[]{0f, 0f, 0f, 0f});
+                    libraw.setCameraWb(true);
+                    libraw.setAutoWb(false);
+                } else if (findViewById(i) == findViewById(R.id.radio_auto_white_balance)) {
+                    libraw.setUserMul(new float[]{0f, 0f, 0f, 0f});
+                    libraw.setCameraWb(false);
+                    libraw.setAutoWb(true);
+                }
             }
         });
-        toneSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                toneOptions.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+        toneRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (findViewById(i) == findViewById(R.id.radio_tone_red)) {
+                    toneSelection = 0;
+                } else if (findViewById(i) == findViewById(R.id.radio_tone_green)) {
+                    toneSelection = 1;
+                } else if (findViewById(i) == findViewById(R.id.radio_tone_blue)) {
+                    toneSelection = 2;
+                }
+            }
+        });
+        curveRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                // TODO: Set curve selection
+                curveSelection = i;
             }
         });
 
-        // White balance Checkboxes
-        cameraWbCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        // Wire bottom menu
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                libraw.setCameraWb(isChecked);
-                if (isChecked) {
-                    autoWbCheck.setChecked(false);
-                    noWbCheck.setChecked(false);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menu_white_balancing) {
+                    whiteBalancingGroup.setVisibility(View.VISIBLE);
+                    toneMappingGroup.setVisibility(View.GONE);
+                    extraGroup.setVisibility(View.GONE);
+                    return true;
+                } else if (item.getItemId() == R.id.menu_tone_mapping) {
+                    whiteBalancingGroup.setVisibility(View.GONE);
+                    toneMappingGroup.setVisibility(View.VISIBLE);
+                    extraGroup.setVisibility(View.GONE);
+                    return true;
+                } else if (item.getItemId() == R.id.menu_extra) {
+                    whiteBalancingGroup.setVisibility(View.GONE);
+                    toneMappingGroup.setVisibility(View.GONE);
+                    extraGroup.setVisibility(View.VISIBLE);
+                    return true;
                 }
-            }
-        });
-        autoWbCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                libraw.setAutoWb(isChecked);
-                if (isChecked) {
-                    cameraWbCheck.setChecked(false);
-                    noWbCheck.setChecked(false);
-                }
-            }
-        });
-        noWbCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    float[] arr = new float[] {1f, 1f, 1f, 1f};
-                    libraw.setUserMul(arr);
-                    cameraWbCheck.setChecked(false);
-                    autoWbCheck.setChecked(false);
-                }
-                else {
-                    float[] arr = new float[] {0f, 0f, 0f, 0f};
-                    libraw.setUserMul(arr);
-                }
+                return false;
             }
         });
 
@@ -164,14 +194,18 @@ public class EditActivity extends AppCompatActivity {
         brightnessSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {}
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 float seekVal = seekBar.getProgress();
                 float bright = 5f / 100.f * seekVal;
-                Toast.makeText(getApplicationContext(),String.valueOf(bright), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), String.valueOf(bright), Toast.LENGTH_SHORT).show();
                 libraw.setBright(bright);
             }
         });
@@ -180,25 +214,31 @@ public class EditActivity extends AppCompatActivity {
         gammaSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {}
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 double seekVal = seekBar.getProgress();
                 double gamma = 6f / 100.f * seekVal + 0.3f;
-                Toast.makeText(getApplicationContext(),String.valueOf(gamma), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), String.valueOf(gamma), Toast.LENGTH_SHORT).show();
                 libraw.setGammPower(1 / gamma);
             }
         });
-        
+
         toneCurveView.setOnTouchListener(new View.OnTouchListener() {
             @SuppressLint("ClickableViewAccessibility")
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    // TODO: Check indices
                     libraw.applyToneCurve(toneCurveView.maxBoundary_x, toneCurveView.maxBoundary_y,
-                            toneCurveView.firstCPArr[2], toneCurveView.secondCPArr[2], toneCurveView.knotsList.get(2), 2);
+                            toneCurveView.firstCPArr[2], toneCurveView.secondCPArr[2],
+                            toneCurveView.knotsList.get(2), toneSelection);
                     processRaw(true);
                 }
                 return false;
